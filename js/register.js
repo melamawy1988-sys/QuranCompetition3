e1bb62b7e84b539ffaa3a97fb5634a9994dc8e6e
melamawy1988-sys <melamawy1988@gmail.com>
2026-06-30 05:56:51 -0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,21 @@
+const select = document.getElementById("parts");
 
+for(let i=1;i<=30;i++){
+
+const option=document.createElement("option");
+
+option.value=i;
+
+option.text=i+" جزء";
+
+select.appendChild(option);
+
+}
+
+document.getElementById("registerForm").addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+alert("تم التحقق من البيانات بنجاح.\nسيتم ربطها بـ Supabase في الجزء القادم.");
+
+});
