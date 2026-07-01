a532ab50f404151bcbb336a5d724eec1a6687b70
melamawy1988-sys <melamawy1988@gmail.com>
2026-07-01 02:48:26 -0700

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,52 +1,8 @@
-// تعبئة قائمة الأجزاء
-const select = document.getElementById("parts");
+alert("تم تحميل register.js");
 
-for (let i = 1; i <= 30; i++) {
-    const option = document.createElement("option");
-    option.value = i;
-    option.textContent = i + " جزء";
-    select.appendChild(option);
-}
+const form = document.getElementById("registerForm");
 
-// عند الضغط على حفظ
-document.getElementById("registerForm").addEventListener("submit", async function (e) {
-
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-
-    const name = document.getElementById("name").value.trim();
-    const age = parseInt(document.getElementById("age").value);
-    const national = document.getElementById("national").value.trim();
-    const parts = parseInt(document.getElementById("parts").value);
-
-    if (national.length !== 14) {
-        alert("الرقم القومي يجب أن يكون 14 رقمًا");
-        return;
-    }
-
-    try {
-
-        const { error } = await db
-            .from("participants")
-            .insert([
-                {
-                    name: name,
-                    age: age,
-                    national_id: national,
-                    parts: parts
-                }
-            ]);
-
-        if (error) {
-            alert("خطأ:\n" + error.message);
-            return;
-        }
-
-        alert("✅ تم حفظ البيانات بنجاح");
-
-        document.getElementById("registerForm").reset();
-
-    } catch (err) {
-        alert(err.message);
-    }
-
+    alert("تم الضغط على زر الحفظ");
 });
