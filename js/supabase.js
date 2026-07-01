48f1c78fd08473e936ecf88b82913ba22651afb5
melamawy1988-sys <melamawy1988@gmail.com>
2026-07-01 02:44:14 -0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 const SUPABASE_URL = "https://ifcynkytrgncpjervddn.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmY3lua3l0cmduY3BqZXJ2ZGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTc1ODMsImV4cCI6MjA5ODM5MzU4M30.tr9olNd_0ka5J7s9AxyhzBjtewlEqub937KBf5RlYzA";
+const SUPABASE_ANON_KEY = "ضع مفتاح anon الحقيقي هنا";
 
-const supabase = window.supabase.createClient(
+const db = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
